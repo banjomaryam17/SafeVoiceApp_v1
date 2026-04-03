68b8b7c5ee837a962995593d70fc44a3ddbf3c86
maryam banjo <mitechnigeria@gmail.com>
2026-04-03 11:29:21 +0100

    Refactor:Added Validation to Login Page -Rerouted deault homepage to Report not Home

--- SafeVoice/Program.cs
+++ SafeVoice/Program.cs
@@ -58,6 +58,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Report}/{action=Index}/{id?}");
 
 app.Run();
