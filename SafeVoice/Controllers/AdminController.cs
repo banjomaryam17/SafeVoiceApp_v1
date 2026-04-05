029740d1cc3dea47d8d8bd91256a52083e80fb25
maryam banjo <mitechnigeria@gmail.com>
2026-04-05 11:32:09 +0100

    Refactor: Moved AdminModel class to a seperate file Created view for Admin Dashboard Refactored UI for index and Details.cshtml

--- SafeVoice/Controllers/AdminController.cs
+++ SafeVoice/Controllers/AdminController.cs
@@ -53,11 +53,3 @@ public class AdminController : Controller
     }
 }
 
-public class AdminDashboardViewModel
-{
-    public int TotalReports { get; set; }
-    public int PendingReports { get; set; }
-    public int TotalUsers { get; set; }
-    public int ActiveSupportLocations { get; set; }
-    public List<Report> RecentReports { get; set; } = new();
-}
